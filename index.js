ae65ab32beead0acfecda8ca031cf51f37f15949
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-22 23:43:13 +0600

    db setup done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -9,21 +10,25 @@ app.use(cors());
 app.use(express.json());
 // ---------------------------
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.66rsk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 // async await function
 async function run() {
-    
-    // try catch finally
-    try {
-      await client.connect();
-      console.log("Database Connected YaY!")
-      // const userCollection = client.db("foodExpress").collection("user");
-        
-    } finally {
-        
-    }
+  // try catch finally
+  try {
+    await client.connect();
+    // const userCollection = client.db("foodExpress").collection("user");
+  } finally {
+  }
 }
 // call function catch (console dir)
-run().catch(console.dir)
+run().catch(console.dir);
 // --------------------------------------------
 
 // http://localhost:5000/
